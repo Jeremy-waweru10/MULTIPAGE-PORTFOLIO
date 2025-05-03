d37c32f2ffdc237f72b1bbdceb83acd25d6cc07b
Jeremy <jeremywawesh10@gmail.com>
2025-05-03 11:24:40 +0300

    Added about component with basic content

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+    return (
+        <section>
+      <h1>About Me</h1>
+      <p>I'm currently learning HTML, CSS, JavaScript and React to build interactive web applications.</p>
+      <h2>My Interests</h2>
+      <ul>
+        <li>Front End Development</li>
+        <li>Programming</li>
+        <li>Web Development</li>
+      </ul>
+    </section>
+        
+    )
+}
+ export default About;
