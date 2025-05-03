7166462969747010f1b1aef1d5f3003da7e8c4ed
Jeremy <jeremywawesh10@gmail.com>
2025-05-03 13:25:33 +0300

    Finalize content for all pages and improve styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //imported components from the components folders
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import { ThemeProvider } from "./components/ThemeContext";
+import "./App.css";
+
+
 
 
 function App() {
   return (
+    
     <Router>
+    <ThemeProvider>
       <div className="app-container">
         <Navbar />
+        <ThemeSwitcher/>
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="/contacts" element={<Contact/>}/>
             
           </Routes>
         </main>
         <Footer />
       </div>
+      </ThemeProvider>
     </Router>
+
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,13 @@ function Home() {
     return (
         <section>
             <h1>Welcome to my portfolio</h1>
-            <p>This is the home page showcasing my work and personality</p>
+            <img src="src\assets\Screenshot (28).png" alt="My profile" className="about-image" />
+            <p>
+          I am Jeremy Waweru, an aspiring software developer passionate about creating innovative solutions.
+          My journey began with a curiosity to solve real-world problems, and that fascination has grown into a strong desire to build impactful software.
+          I'm constantly inspired by the ability to create something from scratch that can help improve people's lives, whether it's a small tool or a large-scale application.
+          My goal is to continuously learn, grow, and challenge myself to be a better developer, staying curious and adaptable in this fast-evolving field.
+          This is the home page showcasing my work and personality.</p>
             </section>
 
     );
