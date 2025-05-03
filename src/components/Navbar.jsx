be02ae9b5ba2a3377919f6a87310aa4fe2f33bd2
Jeremy <jeremywawesh10@gmail.com>
2025-05-03 11:13:36 +0300

    Worked on my Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to = "/">Home</Link>
+            <Link to = "/about">About</Link>
+            <Link to = "/projects">Projects</Link>
+            <Link to = "/contacts">Contacts</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
